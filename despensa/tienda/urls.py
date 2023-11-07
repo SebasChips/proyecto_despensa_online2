e1753578392ef61ddab28824ec1922fff55e41c1
SebasChips <sebastianalarconbejar@gmail.com>
2023-11-06 23:47:19 -0600

    diseño_pagina bootstrap, css y html

--- despensa/tienda/urls.py
+++ despensa/tienda/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.tienda, name ="tienda"),
+    path('tienda/', views.tienda, name ="tienda"),
     path('carro/', views.carro, name ="carro"),
     path('checkout/', views.checkout, name ="checkout"),
 ]
