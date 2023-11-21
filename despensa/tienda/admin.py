3f94d7a0b88e11ad6211c04cfcf1975a766c2d47
Dylan Fher <dylanfercamavelaz@gmail.com>
2023-11-20 22:34:33 -0600

    2 parte de la base de datos pedorra, puse en admin los registros

--- despensa/tienda/admin.py
+++ despensa/tienda/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import*
+
+
+admin.site.register(cliente)
+admin.site.register(Producto)
+admin.site.register(Pedido)
+admin.site.register(orden_producto)
+admin.site.register(Lugar_envio)
